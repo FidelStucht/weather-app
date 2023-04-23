25a1327436ffa4e654d80073b2c0c0da54a82677
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-04-23 09:39:47 +0200

    showing the current temp and humidity

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector(".temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+let apiKey = "370f6ctbab902413o43b492fe5060b44";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Munich&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
